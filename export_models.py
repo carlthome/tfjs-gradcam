b31a23d13f8b93f8cd8ae4a8e0cacf5d1862b6b9
Carl Thomé <carlthome@gmail.com>
2018-08-29 13:25:51 +0200

    Use Python API

--- export_models.py
+++ export_models.py
@@ -6,14 +6,12 @@ import tensorflow as tf
 import tensorflowjs as tfjs
 
 models = inspect.getmembers(tf.keras.applications, inspect.isfunction)
-
 for model_name, model_fn in models:
-    tf.keras.backend.clear_session()
-    model = model_fn()
-    output_path = os.path.join('models', model_name)
-    # TODO Fix FailedPreconditionError
-    #tfjs.converters.save_keras_model(model, output_path)
-    with tempfile.NamedTemporaryFile() as f:
-        model.save(f.name, include_optimizer=False)
-        cmd = f'tensorflowjs_converter --input_format keras {f.name} {output_path}'
-        os.system(cmd)
+    try:
+        tf.keras.backend.clear_session()
+        model = model_fn()
+        model.compile('sgd', 'categorical_crossentropy')
+        output_path = os.path.join('models', model_name)
+        tfjs.converters.save_keras_model(model, output_path)
+    except Exception as ex:
+        print(f'{model_name} could not be converted.', ex)
