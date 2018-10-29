350eb4a708d5159029a2d73c80f65ed7e0b0831a
Carl Thomé <carlthome@gmail.com>
2018-10-29 11:59:07 +0100

    Update

--- test_click.py
+++ test_click.py
@@ -1,22 +1,21 @@
 import os
 
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 
-# TODO Also test with Firefox.
+for driver in [Chrome(), Firefox()]:
+    driver.get('https://carlthome.github.io/tfjs-gradcam')
 
-driver = Chrome('chromedriver')
-driver.get('https://carlthome.github.io/tfjs-gradcam')
+    # Try loading a new image.
+    element = WebDriverWait(driver, 10).until(element_to_be_clickable((By.ID, 'imageButton')))
+    element.click()
 
-# Try loading a new image.
-element = WebDriverWait(driver, 10).until(element_to_be_clickable((By.ID, 'imageButton')))
-element.click()
+    # Try loading another model.
+    element = WebDriverWait(driver, 10).until(
+        element_to_be_clickable((By.ID, 'modelSelect')))
+    select = Select(element)
+    select.select_by_visible_text('DenseNet121')
 
-# Try loading another model.
-element = WebDriverWait(driver, 10).until(element_to_be_clickable((By.ID, 'modelSelect')))
-select = Select(element)
-select.select_by_visible_text('DenseNet121')
-
-driver.quit()
+    driver.quit()
